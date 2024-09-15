e8da589b8d64a56f55114148b83ff42ad9d37abb
Camilo <camilomedina.es@gmail.com>
2024-09-15 16:34:11 -0300

    modificacion en el badge de las tarjetas

--- src/components/common/productCard/ProductCard.jsx
+++ src/components/common/productCard/ProductCard.jsx
@@ -8,7 +8,7 @@ const ProductCard = ({
   contiene,
 }) => {
   return (
-    <div className="card bg-base-100 sm:w-96 w-auto shadow-xl">
+    <div className="card card-compact bg-base-100 sm:w-96 w-auto shadow-xl">
       <figure>
         <img src={image} alt={title} />
       </figure>
@@ -16,10 +16,10 @@ const ProductCard = ({
         <h2 className="card-title text-xl xl:text-2xl">{title}</h2>
         <p className="font-extrabold">${price}</p>
         <p className="font-semibold">Stock: {stock}</p>
-        <p className="font-semibold capitalize">{category}</p>
         <p>{description}</p>
         <p className="text-xs font-bold">{contiene}</p>
-        <div className="card-actions justify-end">
+        <div className="card-actions justify-between">
+          <div className="badge badge-outline capitalize">{category}</div>
           <button className="btn btn-primary">Comprar</button>
         </div>
       </div>
--- src/components/pages/itemListComponents/ItemList.jsx
+++ src/components/pages/itemListComponents/ItemList.jsx
@@ -2,7 +2,7 @@ import ProductCard from "../../common/productCard/ProductCard";
 export const ItemList = ({ items }) => {
   console.log(items);
   return (
-    <div className="grid sm: grid-flow-row grid-col-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 justify-items-center py-2 bg-base-200 sm: px-3">
+    <div className="grid sm: grid-flow-row grid-col-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 justify-items-center py-3 bg-base-200 sm: px-3">
       {items.map((item) => {
         return (
           <ProductCard
