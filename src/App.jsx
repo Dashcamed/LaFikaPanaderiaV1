92a81e2ceef2e2683dd5ff8730283e939ecc2a8e
Camilo <camilomedina.es@gmail.com>
2024-09-25 18:57:09 -0300

    Se agrega pagina item list

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from "./components/layouts/navbar/Navbar";
+import ItemDetailContainer from "./components/pages/itemDetail/ItemDetailContainer";
 import ItemListContainer from "./components/pages/itemListComponents/itemListContainer";
 
 function App() {
   return (
     <div>
       <Navbar />
-      <ItemListContainer />
+      {/* <ItemListContainer /> */}
+      <ItemDetailContainer />
     </div>
   );
 }
